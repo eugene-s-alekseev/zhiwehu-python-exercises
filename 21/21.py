91e959947d9c0abdf9385da262df0620b4e9a9b2
= <=>
2018-02-20 02:03:33 +0300

    changed: 21.py; added tensorflow implementation

--- 21/21.py
+++ 21/21.py
@@ -6,6 +6,7 @@ import argparse
 import itertools
 
 import numpy as np
+import tensorflow as tf
 
 
 class Move:
@@ -33,6 +34,27 @@ class Move:
         return np.linalg.norm(self.location)
 
 
+def get_location(steps):
+    directions = {
+        "up": np.array([1, 0]),
+        "down": np.array([-1, 0]),
+        "right": np.array([0, 1]),
+        "left": np.array([0, -1])
+    }
+    move_tf = tf.placeholder(shape=[2], dtype=tf.float32, name="move")
+    location_tf = tf.Variable(initial_value=[0, 0], dtype=tf.float32, name="location")
+    step_tf = tf.assign_add(location_tf, move_tf, name="make_step")
+    distance_tf = tf.norm(location_tf, name="distance")
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        for step in steps:
+            feed_dict = {move_tf: directions[step]}
+            sess.run(step_tf, feed_dict=feed_dict)
+        result = sess.run(distance_tf)
+
+    return result
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--up", type=int, action="store", dest="up")
@@ -53,6 +75,7 @@ def main():
 
     print(move.distance_)
 
+    print(get_location(args))
 
 if __name__ == "__main__":
     main()
